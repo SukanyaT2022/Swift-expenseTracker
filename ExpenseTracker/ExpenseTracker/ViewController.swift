f7410c12df485e4855198edb8230c29ec603352a
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-12-08 12:05:05 -0500

    add

--- ExpenseTracker/ExpenseTracker/ViewController.swift
+++ ExpenseTracker/ExpenseTracker/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var expenseBaseView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //below make corner raduis
+        expenseBaseView.layer.cornerRadius = 20
+        //add border to black box
+        expenseBaseView.layer.borderWidth = 5.0
+        expenseBaseView.layer.borderColor = UIColor.red.cgColor
     }
 
-
+// how to put hex color also ex size of border 2px
 }
 
