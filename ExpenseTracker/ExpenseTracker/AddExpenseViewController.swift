2b264f08d25272c7cf67af80a0605911651ed6b1
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-12-28 13:08:41 -0500

    add

--- ExpenseTracker/ExpenseTracker/AddExpenseViewController.swift
+++ ExpenseTracker/ExpenseTracker/AddExpenseViewController.swift
@@ -21,24 +21,49 @@ class AddExpenseViewController: UIViewController{
     
     @IBOutlet weak var categoryButton: UIButton!
     
+    
+    @IBOutlet weak var catergoryTextField: UITextField!
+    
     var categoryList : [Category]?//Category come from DataBase
+    
+    var selectedCategoryName = "other" // if user not select anything
     var amountString = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         amountTextField.delegate = self
+        catergoryTextField.delegate = self
         //difference delegate get data--datasource set data
         pickerView.delegate = self
         pickerView.dataSource = self
+        pickerOverlayView.isHidden = true
+        catergoryTextField.isHidden = true
+        getCatergory()
+    }
+    func getCatergory(){
+        let fetchRequest = Category.fetchRequest()//Category is enitity from dataBase
+        if let appDelegate = (UIApplication.shared.delegate as? AppDelegate){
+            if let categories = try? appDelegate.persistentContainer.viewContext.fetch(fetchRequest), categories.count > 0{
+                categoryList = categories
+            }
+        }
     }
     @IBAction func submitButtonAction(_ sender: Any) {
         amountTextField.resignFirstResponder()
     saveExpense()
     }
-    
+    //continue button action
     @IBAction func categoryButtonAction(_ sender: Any) {
+        pickerOverlayView.isHidden = false
+        pickerView.reloadAllComponents()
     }
     
     @IBAction func doneButtonAction(_ sender: Any) {
+        categoryButton.setTitle(selectedCategoryName, for:.normal)
+        pickerOverlayView.isHidden = true
+        if selectedCategoryName == "Other" {
+            catergoryTextField.isHidden = false
+            catergoryTextField.becomeFirstResponder()
+        }
     }
     
     func saveExpense(){
@@ -49,8 +74,10 @@ class AddExpenseViewController: UIViewController{
             entity?.amount = amountString
             //hh hour -mm munite
             entity?.timeStamp = Date.currentDate(dateFormatPattern:"dd-MM-yy HH:mm:ss")
-            entity?.category = "other"
+            entity?.category = selectedCategoryName
             appDelegate.saveContext()//save data to database
+            //line below after fill catergory and $ amount and submit it direct to previous page
+            self.navigationController?.popViewController(animated: true)
         }
     }
 }
@@ -59,11 +86,21 @@ extension AddExpenseViewController: UITextFieldDelegate{
         
     }
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        amountString = textField.text ?? ""  + string
+        if textField == catergoryTextField{
+           selectedCategoryName = textField.text ?? ""  + string
+        }else{
+            amountString = textField.text ?? ""  + string
+        }
+       
         return true
     }
     func textFieldDidEndEditing(_ textField: UITextField) {
-        amountString = textField.text ?? ""
+        if textField == catergoryTextField{
+            selectedCategoryName = textField.text ?? ""
+        }else{
+            amountString = textField.text ?? ""
+        }
+      
     }
 }
 extension AddExpenseViewController: UIPickerViewDelegate,UIPickerViewDataSource{
@@ -76,6 +113,12 @@ extension AddExpenseViewController: UIPickerViewDelegate,UIPickerViewDataSource{
         //how many catergory
         return categoryList?.count ?? 0
     }
-    
-    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categoryList?[row].categoryName
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selecledCategory = categoryList?[row]
+        selectedCategoryName = selecledCategory?.categoryName ?? ""
+        //if user not select anything - it go to catergory ther var line 25
+    }
 }
